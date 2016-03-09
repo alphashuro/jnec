9fdc405918b59a6b85a936248177d53c17349eb0
Alpha Shuro <alphashuro@gmail.com>
2016-03-09 07:24:53 +0200

    fix email, change colors

--- php/send_message.php
+++ php/send_message.php
@@ -1,99 +1,42 @@
 <?php
+  $email_to = "jabu@jnec.co.za";
+  $email_subject = "Website Message";
 
-if(isset($_POST['email'])) {
+  function died($error) {
+    echo "Sorry, but there were error(s) found with the form you submitted. ";
+    echo "These errors appear below.<br /><br />";
+    echo $error."<br /><br />";
+    echo "Please go back and fix these errors.<br /><br />";
+    die();
+  }
 
-    $email_to = "jabu@jnec.co.za";
+  if(!isset($_POST['name']) ||
+    !isset($_POST['email']) ||
+    !isset($_POST['message'])) {
+    died('We are sorry, but there appears to be a problem with the form you submitted.');
+  }
 
-    $email_subject = "Website Message";
+  $name = $_POST['name']; // required
+  $email_from = $_POST['email']; // required
+  $message = $_POST['message']; // not required
 
+  $error_message = "";
+  $email_message = "Form details below.\n\n";
 
+  function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+  }
 
+  $email_message .= "Name: ".clean_string($name)."\n";
+  $email_message .= "Email: ".clean_string($email_from)."\n";
+  $email_message .= "Message: ".clean_string($message)."\n";
 
-
-    function died($error) {
-
-        echo "Sorry, but there were error(s) found with the form you submitted. ";
-
-        echo "These errors appear below.<br /><br />";
-
-        echo $error."<br /><br />";
-
-        echo "Please go back and fix these errors.<br /><br />";
-
-        die();
-
-    }
-
-    if(!isset($_POST['name']) ||
-
-        !isset($_POST['email']) ||
-
-        !isset($_POST['message']) {
-
-        died('We are sorry, but there appears to be a problem with the form you submitted.');
-
-    }
-
-
-
-    $name = $_POST['name']; // required
-
-    $email_from = $_POST['email']; // required
-
-    $message = $_POST['message']; // not required
-
-
-
-    $error_message = "";
-
-    $email_message = "Form details below.\n\n";
-
-
-
-    function clean_string($string) {
-
-      $bad = array("content-type","bcc:","to:","cc:","href");
-
-      return str_replace($bad,"",$string);
-
-    }
-
-
-
-    $email_message .= "Name: ".clean_string($name)."\n";
-
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-
-    $email_message .= "Message: ".clean_string($message)."\n";
-
-
-
-
-
-// create email headers
-
-$headers = 'From: '.$email_from."\r\n".
-
-'Reply-To: '.$email_from."\r\n" .
-
-'X-Mailer: PHP/' . phpversion();
-
-@mail($email_to, $email_subject, $email_message, $headers);
-
+  // create email headers
+  $headers = 'From: '.$email_from."\r\n".
+  'Reply-To: '.$email_from."\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+  @mail($email_to, $email_subject, $email_message, $headers);
 ?>
 
-
-
-<!-- include your own success html here -->
-
-
-
-Thank you for contacting us. We will be in touch with you very soon.
-
-
-
-<?php
-
-}
-
-?>
+Message sent. Thank you for contacting us. We will be in touch with you very soon.
---
+++ start.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+cd assets/css && stylus -w
